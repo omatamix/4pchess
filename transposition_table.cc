cc49022351540acda3550b1440627da776804ff4
Nicholas <47761650+omatamix@users.noreply.github.com>
2025-04-05 10:35:44 -0700

    cs cleanup

--- transposition_table.cc
+++ transposition_table.cc
@@ -15,33 +15,39 @@ TranspositionTable::TranspositionTable(size_t table_size) {
       "Can't create transposition table. Try using a smaller size.");
 }
 
-const HashTableEntry* TranspositionTable::Get(int64_t key) {
+const HashTableEntry* TranspositionTable::Get(
+  int64_t key
+) {
   size_t n = key % table_size_;
   HashTableEntry* entry = hash_table_ + n;
-  if (entry->key == key) {
+  if (entry->key == key)
+  {
     return entry;
   }
   return nullptr;
 }
 
 void TranspositionTable::Save(
-    int64_t key, int depth, std::optional<Move> move, int score, int eval,
-    ScoreBound bound, bool is_pv) {
+  int64_t key,
+  int depth,
+  std::optional<Move> move,
+  int score,
+  int eval,
+  ScoreBound bound,
+  bool is_pv
+) {
   size_t n = key % table_size_;
   HashTableEntry& entry = hash_table_[n];
-  if (bound == EXACT
-      || entry.key != key
-      || entry.depth < depth) {
-    entry.key = key;
+  if (bound == EXACT || entry.key != key || entry.depth < depth)
+  {
+    entry.key   = key;
     entry.depth = depth;
-    entry.move = move;
+    entry.move  = move;
     entry.score = score;
-    entry.eval = eval;
+    entry.eval  = eval;
     entry.bound = bound;
     entry.is_pv = is_pv;
   }
 }
-
-
 }  // namespace chess
 
